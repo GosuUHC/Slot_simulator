a4c4c3fcb1bdd8a10c2e78f86228dd79e14c5a62
Gosu <GosuUchenik@yandex.ru>
2020-11-15 19:53:33 +0700

    Initialization of variable

---
+++ SLot_test_1/SLot_test_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SLot_test_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pos1, pos2, pos3;
+            int dev1, dev2, dev3;
+            int des1, des2, des3;
+            int j1, j2, j3;
+            int q1, q2, q3;
+            int k1, k2, k3;
+            int a1, a2, a3;
+            double win, sum, bet;
+
+        }
+    }
+}
